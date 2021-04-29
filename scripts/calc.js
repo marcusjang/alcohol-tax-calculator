023b9fa3764a7bcbd4e42f45d65b81cad661548b
Marcus Jang <makerpopo@gmail.com>
2021-04-29 15:28:30 +0900

    Update calc.js

--- scripts/calc.js
+++ scripts/calc.js
@@ -7,29 +7,29 @@ class Quote {
         dollarRate: { value: dollarRate }, 
         isFTA: { checked: isFTA }
     }) {
-    const parcelPrice = [
-        18500,
-        24500,
-        30500,
-        37000,
-        43000
-    ];
-  	const prelim = (total*1 + shipping*1) * rate;
-    
-  	this.baseline = this.trunc(((total * rate) <= 200000) ? (total * rate) + parcelPrice[bottles-1] : prelim);
-    this.hasAdditTax = (this.baseline / dollarRate >= 150) || bottles > 1;
-	this.tariff = (this.hasAdditTax && !isFTA) ? this.trunc(this.baseline * 0.2) : 0;
-    this.alcoholTax = this.trunc((this.baseline + this.tariff) * 0.72);
-    this.eduTax = this.trunc(this.alcoholTax * 0.3);    
-    this.vat = (this.hasAdditTax) ? this.trunc((this.baseline + this.tariff + this.alcoholTax + this.eduTax) * 0.1) : 0;
+        const parcelPrice = [
+            18500,
+            24500,
+            30500,
+            37000,
+            43000
+        ];
+        const prelim = (total*1 + shipping*1) * rate;
+
+        this.baseline = this.trunc(((total * rate) <= 200000) ? (total * rate) + parcelPrice[bottles-1] : prelim);
+        this.hasAdditTax = (this.baseline / dollarRate >= 150) || bottles > 1;
+        this.tariff = (this.hasAdditTax && !isFTA) ? this.trunc(this.baseline * 0.2) : 0;
+        this.alcoholTax = this.trunc((this.baseline + this.tariff) * 0.72);
+        this.eduTax = this.trunc(this.alcoholTax * 0.3);    
+        this.vat = (this.hasAdditTax) ? this.trunc((this.baseline + this.tariff + this.alcoholTax + this.eduTax) * 0.1) : 0;
+
+        this.totalTax = this.tariff + this.alcoholTax + this.eduTax + this.vat;
+        this.total = this.trunc(prelim + this.totalTax);
+    }
   
-    this.totalTax = this.tariff + this.alcoholTax + this.eduTax + this.vat;
-    this.total = this.trunc(prelim + this.totalTax);
-  }
-  
-  trunc(num) {
-  	return Math.trunc(num / 10) * 10;
-  }
+    trunc(num) {
+        return Math.trunc(num / 10) * 10;
+    }
 }
 
 document.addEventListener("DOMContentLoaded", event => {
