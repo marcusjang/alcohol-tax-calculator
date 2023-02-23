8523a973d82a2f7b91df330e8b24e749c202d996
Marcus Jang <makerpopo@gmail.com>
2023-02-23 20:56:31 +0900

    Add basic dynamic forex data fetching

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -6,58 +6,66 @@ const parcelPrice = [
 	[13000, 20000, 27500, 35500, 43000, 50500, 58500, 66000, 74000, 81500, 89000]
 ];
 
-const rates = {
-	"EUR": 1363.3,
-	"USD": 1274.36,
-	"GBP": 1538.63,
-	"DKK": 183,
-	"JPY": 9.5808,
-	"KRW": 1
+const rateDataUrl = 'https://gist.githubusercontent.com/marcusjang/ae44a296cad4df07a12c4e211ded4871/raw/bf6c0b60456d0f0b21a4c28d2fd95bfc2856ec96/rates.json';
+const countryDataUrl = 'https://gist.githubusercontent.com/marcusjang/51253d9124a75e846ff78331415674ac/raw/3d01a7091ffb808170033e6322542953a0c8d558/country_data.json';
+const countryPriority = [ 'GB', 'EU', 'US', 'DK', 'JP' ];
+
+// source: https://alanedwardes.com/blog/posts/country-code-to-flag-emoji-csharp/
+function isoCountryCodeToFlagEmoji(country) {
+	return String.fromCodePoint(...[...country.toUpperCase()].map(c => c.charCodeAt() + 0x1F1A5));
 }
 
-const countryData = [{
-		"country_code": "EU",
-		"country_name": "유럽연합",
-		"currency_code": "EUR",
-		"currency_name": "유로"
-	},
-	{
-		"country_code": "US",
-		"country_name": "미국",
-		"currency_code": "USD",
-		"currency_name": "달러"
-	},
-	{
-		"country_code": "GB",
-		"country_name": "영국",
-		"currency_code": "GBP",
-		"currency_name": "파운드"
-	},
-	{
-		"country_code": "DK",
-		"country_name": "덴마크",
-		"currency_code": "DKK",
-		"currency_name": "크로네"
-	},
-	{
-		"country_code": "JP",
-		"country_name": "일본",
-		"currency_code": "JPY",
-		"currency_name": "엔"
-	},
-	{
-		"country_code": "KR",
-		"country_name": "대한민국",
-		"currency_code": "KRW",
-		"currency_name": "원"
-	}
-]
 
+async function getData() {
+	const key = 'country-data';
+	//localStorage.removeItem(key);
+	let data = JSON.parse(localStorage.getItem(key));
+	if (!data) {
+		console.log('no local data found, fetching remote...');
+		data = await fetchData();
+		localStorage.setItem(key, JSON.stringify(data));
+	}
+	return data;
+}
+
+function fetchData() {
+	return Promise.all([
+		fetch(rateDataUrl).then(res => res.json()),
+		fetch(countryDataUrl).then(res => res.json())
+	]).then(data => {
+		return data[1].sort((a, b) => {
+			const aCode = a.country_code;
+			const bCode = b.country_code;
+			const aIndex = countryPriority.findIndex(code => code === aCode);
+			const bIndex = countryPriority.findIndex(code => code === bCode);
+			if (aIndex === -1) return 1;
+			if (bIndex === -1) return -1;
+			return aIndex - bIndex;
+		}).map(country => {
+			country.rate = data[0][country.currency_code];
+			country.flag = isoCountryCodeToFlagEmoji(country.country_code);
+			return country;
+		});
+	});
+}
+
+function renderOption(country) {
+	const option = document.createElement('option');
+	option.innerText = `${ country.flag } ${ country.country_name } ${ country.currency_name }`;
+	option.value = country.rate;
+	option.dataset.region = country.region;
+	option.dataset.currency = country.currency_code;
+	option.dataset.country = country.country_code;
+
+	return option;
+}
 
 class QuoteForm {
 	constructor(form) {
 		this.form = form;
 		this.elements = this.form.elements;
+		this.rate = 1500;
+		this.rateUSD = 1200; // fallback
 		this.#initForm();
 	}
 	
@@ -84,7 +92,7 @@ class QuoteForm {
 					this.render();
 				});
 			} else if (tagName === 'SELECT' && name === 'currency') {
-				elem.addEventListener('change', () => this.#setCurrency());
+				elem.addEventListener('change', () => this.setCurrency());
 			} else if (tagName === 'INPUT' && elem.type === 'text') {
 				elem.addEventListener('input', () => this.render());
 			} else if ((tagName === 'INPUT' && elem.type === 'checkbox') ||
@@ -94,46 +102,68 @@ class QuoteForm {
 		}
 
 		this.form.addEventListener('update-price', event => {
-			this.price = event.detail.value * this.rate;
 			
-			for (const elem of event.currentTarget.elements)  {
-				if (elem.tagName === 'INPUT' && elem.dataset.type === 'price' &&
-					elem !== event.detail.originalTarget) {
-					const value = this.price / rates[elem.dataset.currency];
-					if (!isNaN(value)) {
-						elem.value = QuoteForm.trunc(value);
-					}
-				}
-			}
+			const originalTarget = event.detail.originalTarget;
+			
+			if (originalTarget === this.elements.price)
+				this.price = event.detail.value * this.rate;
+			else if (originalTarget === this.elements['price-usd'])
+				this.price = event.detail.value * this.rateUSD;
+			else if (originalTarget === this.elements['price-krw'])
+				this.price = event.detail.value;
+			
+			if (originalTarget !== this.elements.price)
+				this.elements.price.value = QuoteForm.trunc(this.price / this.rate);
+			
+			if (originalTarget !== this.elements['price-usd'])
+				this.elements['price-usd'].value = this.priceUSD;
+			
+			if (originalTarget !== this.elements['price-krw'])
+				this.elements['price-krw'].value = QuoteForm.trunc(this.price);
+
 			this.render();
 		}, true);
 		
-		this.#setCurrency();
+		this.setCurrency();
 	}
 
-	#setCurrency() {
-		this.currency = this.elements.currency.value;
-		this.rate = rates[this.currency];
-		this.elements.price.dataset.currency = this.currency;
+	setCurrency() {
+		const select = this.elements.currency;
+		const index = select.selectedIndex;
+		const selected = select.options[index];
+		
+		this.rate = parseFloat(select.value);
+		this.country = selected.dataset.country;
+		this.currency = selected.dataset.currency;
+		this.region = parseInt(selected.dataset.region);
+		
 		this.elements.price.dispatchEvent(new InputEvent('input'));
 	}
 	
 	render() {
-		this.elements['tax-total'].value = this.totalTax;
-		this.elements['tax-alcohol'].value = this.alcoholTax;
-		this.elements['tax-edu'].value = this.eduTax;
-		this.elements['tax-tariff'].value = this.tariff;
-		this.elements['tax-vat'].value = this.vat;
+		(this.over150USD) ? this.elements['over-150-usd'].classList.add('active') : this.elements['over-150-usd'].classList.remove('active');
+		(this.over200kKRW) ? this.elements['over-200k-krw'].classList.add('active') : this.elements['over-200k-krw'].classList.remove('active');
+		
+		this.elements['tax-total'].value = this.totalTax.toLocaleString();
+		this.elements['tax-alcohol'].value = this.alcoholTax.toLocaleString();
+		this.elements['tax-edu'].value = this.eduTax.toLocaleString();
+		this.elements['tax-tariff'].value = this.tariff.toLocaleString();
+		this.elements['tax-vat'].value = this.vat.toLocaleString();
+		this.elements['total'].value = this.total.toLocaleString();
 	}
 	
 	get shipping() {
 		const input = parseFloat(this.elements.shipping.value);
-		return (this.over200kKRW) ? QuoteForm.trunc(input * this.rate) : 18500;
+		return QuoteForm.trunc(input * this.rate);
+	}
+	
+	get shippingTax() {
+		return (this.over200kKRW) ? this.shipping : 18500;
 		// todo: variable shipping price according to region and/or weight
 	}
 	
 	get priceUSD() {
-		return QuoteForm.trunc(this.price / rates['USD']);
+		return QuoteForm.trunc(this.price / this.rateUSD);
 	}
 	
 	get over150USD() {
@@ -153,7 +183,7 @@ class QuoteForm {
 	}
 	
 	get baseline() {
-		return (this.includeShipping) ? this.price + this.shipping : this.price;
+		return (this.includeShipping) ? this.price + this.shippingTax : this.price;
 	}
 	
 	get tariff() {
@@ -182,18 +212,37 @@ class QuoteForm {
 		return this.tariff + this.alcoholTax + this.eduTax + this.vat;
 	}
 	
+	get total() {
+		return QuoteForm.floorTen(this.price + this.shipping + this.totalTax);
+	}
+	
 	static trunc(number) {
-		return Math.floor(number * 100) / 100;
+		const value = Math.floor(number * 100) / 100;
+		return (isNaN(value)) ? 0 : value;;
 	}
 	
 	static floorTen(number) {
-		return Math.floor(number / 10) * 10;
+		const value = Math.floor(number / 10) * 10;
+		return (isNaN(value)) ? 0 : value;;
 	}
 }
 
 function init() {
 	const form = document.forms[0];
+	
 	const quote = new QuoteForm(form);
+	
+	getData().then(data => {
+		const select = quote.elements.currency;
+		quote.rates = data;
+		quote.rateUSD = data.find(datum => datum.currency_code === 'USD').rate;
+		select.innerHTML = '';
+		for (const country of data) {
+			const option = renderOption(country);
+			select.append(option)
+		}
+		quote.setCurrency();
+	});
 }
 
 document.addEventListener('DOMContentLoaded', init);
